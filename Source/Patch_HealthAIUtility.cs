f2abbe6bbef9f18ea5fc4634085a47b4803e87fc
1trickPonyta <1trickPonyta@kangaroostandard.com>
2021-08-06 15:23:42 -0700

    initial

---
+++ Source/Patch_HealthAIUtility.cs
@@ -0,0 +1,26 @@
+﻿using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace BedRestForFoodPoisoning
+{
+    [HarmonyPatch(typeof(HealthAIUtility))]
+    [HarmonyPatch("ShouldSeekMedicalRest")]
+    public static class Patch_HealthAIUtility_ShouldSeekMedicalRest
+    {
+        public static void Postfix(ref Pawn pawn, ref bool __result)
+        {
+            if (!__result)
+            {
+                Hediff hediff;
+                if ((hediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning)) != null)
+                {
+                    if (hediff.CurStage.label == "initial" || hediff.CurStage.label == "major")
+                    {
+                        __result = true;
+                    }
+                }
+            }
+        }
+    }
+}
